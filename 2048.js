42c731086796c43a513b432a769df09e12b57f02
Eternal-CD <77340079+Eternal-CD@users.noreply.github.com>
2021-11-20 22:47:31 +0300

    Add files via upload

---
+++ 2048.js
@@ -0,0 +1,11 @@
+let fieldPlay = document.getElementById('fieldPlay');
+let ctx = fieldPlay.getContext('2d');
+let field = document.getElementById('field');
+field = 5;
+let button = document.getElementById('button')
+let width = fieldPlay.width / field - 6;
+
+console.log(width);
+
+
+
